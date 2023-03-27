c534dfdb7be44ce113238bdb837c042265ca857b
Fhawk01 <78235151+Fhawk01@users.noreply.github.com>
2023-03-26 19:34:48 -0500

    2 files modified

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const formController = require('../controllers/form-controller');
+const formController = require('../controllers/formController');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
